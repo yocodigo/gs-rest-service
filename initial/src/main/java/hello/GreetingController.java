95983ca9679865195b7161a98cb4dcf1a11f7cef
Joel Hernandez <joelhernandez@Joels-MBP.attlocal.net>
2019-09-24 13:42:59 -0700

    Adding saludos method to GreetingController

--- initial/src/main/java/hello/GreetingController.java
+++ initial/src/main/java/hello/GreetingController.java
@@ -1,19 +1,27 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/greeting/spanish")
+    public Greeting saludos(@RequestParam(value="nombre", defaultValue="Mundo") String nombre) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, nombre));
     }
 
 }
